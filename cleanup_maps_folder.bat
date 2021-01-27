001ed86b2a2aa46ae342203705704c79229d2824
MFG38 <mfg38thelps@aol.com>
2021-01-27 11:07:20 +0200

    Added cleanup_maps_folder.bat + modified compile.bat

---
+++ cleanup_maps_folder.bat
@@ -0,0 +1,8 @@
+@echo off
+
+cd src\maps
+echo Deleting .dbs and .backup# files...
+del *.dbs
+del *.wad.backup1
+del *.wad.backup2
+del *.wad.backup3
--- compile.bat
+++ compile.bat
@@ -1,10 +1,20 @@
 @echo off
 
+echo Remember to run cleanup_maps_folder.bat before compiling!
+echo Is the project ready to be compiled?
+set /p c=Enter Y to compile or N to cancel.
+if /i "%c" == "Y" goto :Compile 
+if /i "%c" == "N" goto :Cancel
+
+:Compile
 cd src
 echo Compiling...
 ..\tools\7za a -tzip absolvum.pk3 *.* *
 move absolvum.pk3 ../absolvum.pk3
 goto Done
+
+:Cancel
+exit
 
 :Done
 pause >nul
